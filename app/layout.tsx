ea7a4207c051390bb8fafc29a1bd0b33aa46cb7e
TechnoActive <info@technoactive.co.uk>
2026-01-16 11:21:39 +0000

    Add Google Tag Manager (GTM-NT4HDF3X) for SEO tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -322,6 +322,18 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} ${playfair.variable}`}>
       <head>
+        {/* Google Tag Manager - Must be as high in head as possible */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NT4HDF3X');`,
+          }}
+        />
         <Script
           id="json-ld-website"
           type="application/ld+json"
@@ -367,6 +379,15 @@ export default function RootLayout({
         />
       </head>
       <body className="font-sans antialiased">
+        {/* Google Tag Manager (noscript) - Must be immediately after opening body tag */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NT4HDF3X"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         {children}
         <CookieBanner />
         <CookieSettingsButton />
